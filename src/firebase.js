6c92f47f93e2bc531e40950f21855929f149d965
bbj007 <bbj0073@naver.com>
2020-11-29 23:26:08 +0900

    20201129 - React & Firebase Set-up

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZep6eOtnLEQg-rPcPxfjcCwk3Wx85Rtk",
+    authDomain: "nwitter-413ff.firebaseapp.com",
+    databaseURL: "https://nwitter-413ff.firebaseio.com",
+    projectId: "nwitter-413ff",
+    storageBucket: "nwitter-413ff.appspot.com",
+    messagingSenderId: "206620826741",
+    appId: "1:206620826741:web:3b5f60c51c384473652c3c"
+};
+
+export default firebaseConfig.initializeApp(firebaseConfig);
