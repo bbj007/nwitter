48573f6c4aa4dc987dbb9a7f753582c25ad67853
bbj007 <bbj0073@naver.com>
2020-12-06 01:21:13 +0900

    20201206 - Get the data from firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
 import { dbService } from 'fbase'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
   const [nweet, setNweet] = useState('')
+  const [nweets, setNweets] = useState([])
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection('nweets').get()
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      }
+      setNweets((prev) => [document.data(), ...prev])
+    })
+    console.log(dbNweets)
+  }
+
+  useEffect(() => {
+    getNweets()
+  }, [])
 
   const onSubmit = (event) => {
     event.preventDefault()
@@ -12,6 +28,7 @@ const Home = () => {
     })
     setNweet('')
   }
+
   const onChange = (event) => {
     const {
       target: { value },
@@ -31,6 +48,13 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
